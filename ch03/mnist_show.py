a76562c2022afc6e4d557f1e9b5f93a3386bddf5
lsjhome007 <lsjhome007@gmail.com>
2017-09-20 10:54:00 +0900

    Update mnist_show.py
    잘 안되어서 이렇게 해 보니 보입니다. Pillow는 python3에서 설치하였습니다.

--- ch03/mnist_show.py
+++ ch03/mnist_show.py
@@ -3,12 +3,13 @@ import sys, os
 sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 import numpy as np
 from dataset.mnist import load_mnist
-from PIL import Image
+from PIL import Image # pip install Pillow 를 통해서 설치
 
 
 def img_show(img):
     pil_img = Image.fromarray(np.uint8(img))
-    pil_img.show()
+    #pil_img.show()
+    return pil_img
 
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
 
